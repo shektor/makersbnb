6f8315b2fe30918f29314f7897f4481050b6d92b
MaxRobertsDear <maxhroberts@gmail.com>
2019-05-07 16:48:30 +0100

    add button that takes user from /spaces to /spaces/new

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const port = 4567
 
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => res.redirect('/spaces'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-app.get('/spaces', (req, res) => res.send('Hello Gavin'))
+app.get('/spaces', (req, res) => res.render('spaces', { title: "Spaces", message: "Book a Space", button: "List a Space" }))
 
-// app.get('/spaces/new')
+
+app.get('/spaces/new')
