398724d9865c9ed29fb407956053df5ea9b2a639
MaxRobertsDear <maxhroberts@gmail.com>
2019-05-08 16:56:41 +0100

    user stories 2 and 3: list multiple spaces with details

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 4567
 const Space = require('./space')
 var bodyParser = require('body-parser')
+var spaces = []
 
 // function Space() {
 //   this.name
@@ -15,7 +16,6 @@ var jsonParser = bodyParser.json()
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-var space = new Space()
 
 app.set('view engine', 'pug')
 
@@ -23,15 +23,17 @@ app.get('/', (req, res) => res.redirect('/spaces'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-app.get('/spaces', (req, res) => res.render('spaces/index', { button: "List a Space", space: space }))
+app.get('/spaces', (req, res) => res.render('spaces/index', { button: "List a Space", spaces: spaces }))
 // name: name, description: description, price: price
 
 app.get('/spaces/new', (req, res) => res.render('spaces/new', { message: "List your Space" }))
 
 
 app.post('/spaces', urlencodedParser, (req, res) => {
+  var space = new Space()
   space.name = req.body.name
   space.description = req.body.description
   space.price = req.body.price
+  spaces.push(space)
   res.redirect('/spaces')
 })
