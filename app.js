73ee63170089f435e9881730d6d99f9f2f789941
MaxRobertsDear <maxhroberts@gmail.com>
2019-05-08 16:59:25 +0100

    add src directory

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 4567
-const Space = require('./space')
+const Space = require('./src/space')
 var bodyParser = require('body-parser')
 var spaces = []
 
